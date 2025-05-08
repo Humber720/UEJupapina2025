be9272de110b3b1d106564cdb36c1748162875df
Humber720 <yupconhum321@gmail.com>
2025-05-07 21:31:31 -0400

    plataforma.js

--- plataforma.js
+++ plataforma.js
@@ -1476,29 +1476,15 @@ function openReport() {
 }
 
 // Función para cerrar sesión
-// Función para cerrar sesión sin poder regresar con el botón "Atrás"
 function logout() {
-    localStorage.removeItem("loggedUser");  // Elimina el usuario de la sesión
-    window.location.replace("index.html");  // Redirige a la página de inicio y evita que el usuario regrese a la página de plataforma
+    localStorage.removeItem("loggedUser");
+    window.location.href = "index.html";
 }
 
-// Llama a loadGrades solo si estamos en plataforma.html y hay un usuario autenticado
+// Llama a loadGrades solo si estamos en plataforma.html
 if (window.location.pathname.includes("plataforma.html")) {
-    const username = localStorage.getItem("loggedUser");
-    if (username) {
-        loadGrades();  // Carga las calificaciones solo si el usuario está autenticado
-    } else {
-        window.location.replace("index.html");  // Redirige a la página de inicio si no hay sesión activa
-    }
+    loadGrades();
 }
-
-// Protección de acceso en la plataforma: Redirige si no hay sesión activa
-window.addEventListener("pageshow", function () {
-    const username = localStorage.getItem("loggedUser");
-    if (!username) {
-        window.location.replace("index.html");  // Redirige a la página de inicio si no hay sesión activa
-    }
-});
 
 
 
